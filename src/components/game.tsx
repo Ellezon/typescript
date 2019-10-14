a1d0e91a65ec8a5d0760b8f1666ac8f58e215a16
Ellezon <mikkifalz@gmail.com>
2019-10-14 20:22:05 +0200

    Added Browser router, Home and Game components

---
+++ src/components/game.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+type GameProps = {
+    game:
+    {
+        id: number;
+        name: string;
+        play_url: string;
+        logo_url: string;
+    };
+}
+const Game:React.FC<GameProps> = ({game}) => {
+    return (
+        <div className="game-panel col-12 col-md-6 col-lg-3" key={game.id}>
+        <a href={game.play_url} target="_blank" rel="noopener noreferrer">
+          <h4>{game.name}</h4>
+          <img src={game.logo_url} alt={game.name} />
+        </a>
+      </div>
+    )
+
+}
+export default Game;
---
+++ src/components/home.tsx
@@ -0,0 +1,58 @@
+import React, { useState, useEffect  } from "react";
+import Header from "./header";
+import Footer from "./footer";
+import Game from "./game";
+
+
+type HomeProps = {
+    games: Array<any>;
+};
+
+const Home: React.FC<HomeProps> = ({games}) =>
+{
+    const [allGames, setGames] = useState<Array<any>>([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(false);
+   
+    // Similar to componentDidMount and componentDidUpdate:
+    useEffect(() => {
+        setIsLoading(true);
+        if (games) {
+            setGames(games);
+            setIsLoading(false);
+        } 
+        else {
+          fetch("/games/get")
+            .then(res => res.json())
+            .then(data => {
+              if (data) {
+                setGames(data);
+                setIsLoading(false);
+              }
+            })
+            .catch(error => {
+                setError(error.message);
+                setIsLoading(false);
+            });
+        }
+    }, []);
+
+    return (
+        <React.Fragment>
+          <Header />
+          <h1 className="col-12 title">Games</h1>
+          <div className="container main-container">
+            <div className="row game-section">
+                {isLoading && !error && <span> Loading...</span>}
+                {!isLoading && !error && allGames.length > 0 &&
+                allGames.map((game: { id: number; play_url: string; name: string; logo_url: string }) => {
+                 return <Game game={game}/>
+                })}
+                {error && <span> Error! {error}</span>}
+            </div>
+          </div>
+          <Footer/>
+        </React.Fragment>
+      );
+}
+export default Home;
