ecf5b88d650c924466d264f20b005be5715cff17
Ellezon <mikkifalz@gmail.com>
2019-10-14 20:20:33 +0200

    Added footer and header

---
+++ src/components/footer.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+    return (
+        <footer>
+            <div className="container">
+                <div className="row">
+                    <div className="footer-content col-12">Copyright© 2019</div>
+                </div>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
+
