1363e5438fc7f3df7df329e90fb7eb6e46ee6eb6
Ellezon <mikkifalz@gmail.com>
2019-10-27 14:39:09 +0100

    Connect App to redux store

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,21 @@ import React from "react";
 import Home from "./components/home";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./styles/css/main.css";
-function App () {
-    return (
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/" component={Home} />
-          </Switch>
-        </BrowserRouter>
-    );
+import { configureStore } from "./redux/index";
+import { Provider } from "react-redux";
+
+const store = configureStore();
+
+function App() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
+    </Provider>
+  );
 }
 
 export default App;
