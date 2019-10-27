f6accac50763f07b0d9d73864187fad0da2c1753
Ellezon <mikkifalz@gmail.com>
2019-10-27 17:06:23 +0100

    Added code for chrome dev tool redux extension

--- src/redux/index.tsx
+++ src/redux/index.tsx
@@ -1,13 +1,12 @@
-import { createStore } from "redux";
+import { createStore  } from "redux";
 import { reducer } from "./reducers";
+import { devToolsEnhancer } from 'redux-devtools-extension';
 
 export type AppState = ReturnType<typeof reducer>;
 
 export function configureStore() {
-  const store = createStore(
-    reducer
- );
-  
-  return store;
+
+    const store = createStore(reducer,  devToolsEnhancer({}));
+    return store;
 }
 
